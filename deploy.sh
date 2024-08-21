02503e4d032f3aaf3919719a559189b8434e45ed
Achyut Luitel <99021369+cryptic333@users.noreply.github.com>
2024-08-21 13:39:28 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+bash apache-install.sh
+
+sudo cp apache-config.conf /etc/apache2/sites-available/000-default.conf
+sudo systemctl reload apache2
+
+bash ssl-setup.sh
+
+bash firewall-setup.sh
+
+bash security-hardening.sh
+
+echo "Apache setup and security completed."
